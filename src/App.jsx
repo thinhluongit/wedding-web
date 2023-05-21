63b505ebb95b40723c9f44ecd249069493440ac2
LuongThinh0902 <125153337+LuongThinh0902@users.noreply.github.com>
2023-05-21 10:22:37 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <Footer/>
+  );
+}
+
+export default App;
---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from './footer.module.css'
+
+const Footer = () => {
+    return(
+        <div className={css.container}>
+            
+        </div>
+    )
+}
+
+export default Footer;
